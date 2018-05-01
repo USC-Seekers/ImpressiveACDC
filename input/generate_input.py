6566fe70a64e28b21dde9ec5a2b588c457780f47
Alston Tang <tanghaomo@gmail.com>
2018-05-01 12:11:16 -0700

    Update json generator

--- input/generate_input.py
+++ input/generate_input.py
@@ -1,4 +1,4 @@
-from preprocess import read_cluster, read_deps, anal_cluster_deps, write_json
+from preprocess import read_cluster, read_deps, anal_cluster_deps, write_cluster_json
 from consistent import generate
 
 files = [
@@ -6,7 +6,8 @@ files = [
      "relax": "log4j-2.1_relax_clusters.rsf"},
     {"acdc": 'log4j-2.4_acdc_clustered.rsf', "dep": 'log4j-2.4_deps.rsf', "name": 'log4j-2.4',
      "relax": "log4j-2.4_relax_clusters.rsf"},
-    {"acdc": 'hadoop-0.2.0_clusters.rsf', "dep": 'hadoop-0.2.0_class_deps.rsf', "name": 'hadoop-0.2.0'}
+    {"acdc": 'hadoop-0.2.0_acdc_clustered.rsf', "dep": 'hadoop-0.2.0_deps.rsf', "name": 'hadoop-0.2.0',
+     "relax": 'hadoop-0.2.0_relax_clusters.rsf'}
 ]
 
 for version in files:
@@ -16,10 +17,10 @@ for version in files:
     name = version.get("name", None)
 
     if acdc is not None and dep is not None and name is not None:
-        m, cnt = read_cluster(acdc)
+        m, cnt, _ = read_cluster(acdc)
         d = read_deps(dep)
         adj = anal_cluster_deps(m, d)
-        write_json(name + ".json", cnt, adj)
+        write_cluster_json(name + ".json", cnt, adj)
 
     if acdc is not None and dep is not None and name is not None and relax is not None:
         generate(acdc, relax, dep, name + "-consistent.json")
