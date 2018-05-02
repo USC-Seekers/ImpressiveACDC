d3355cfec4cdeb3d01ec0dc9e97db74e16cea2ef
Alston Tang <tanghaomo@gmail.com>
2018-05-01 18:03:48 -0700

    Add complexity to class json

--- input/class_generator.py
+++ input/class_generator.py
@@ -3,7 +3,7 @@ from RELAX import RELAX
 import json
 
 
-def generate_class_json(acdc_cluster, relax_cluster, out_file):
+def generate_class_json(acdc_cluster, relax_cluster, ucc_size, out_file):
     acdc = ACDC(acdc_cluster)
     relax = RELAX(relax_cluster)
 
@@ -17,6 +17,7 @@ def generate_class_json(acdc_cluster, relax_cluster, out_file):
             package_rv["name"] = package
             package_type = relax.packages.get(package, None)
             package_rv["type"] = package_type
+            package_rv["complexity"] = ucc_size.get(package, None)
             rv[cluster_name].append(package_rv)
 
     f = open(out_file, "w")
--- input/generate_input.py
+++ input/generate_input.py
@@ -1,14 +1,14 @@
-from preprocess import read_cluster, read_deps, anal_cluster_deps, write_cluster_json
+from preprocess import read_cluster, read_deps, anal_cluster_deps, write_cluster_json, read_ucc_output
 from consistent import generate
 from class_generator import generate_class_json
 
 files = [
     {"acdc": 'log4j-2.1_acdc_clustered.rsf', "dep": 'log4j-2.1_deps.rsf', "name": 'log4j-2.1',
-     "relax": "log4j-2.1_relax_clusters.rsf"},
+     "relax": "log4j-2.1_relax_clusters.rsf", "ucc": "log4j-2.1-Java_outfile.csv"},
     {"acdc": 'log4j-2.4_acdc_clustered.rsf', "dep": 'log4j-2.4_deps.rsf', "name": 'log4j-2.4',
      "relax": "log4j-2.4_relax_clusters.rsf"},
     {"acdc": 'hadoop-0.2.0_acdc_clustered.rsf', "dep": 'hadoop-0.2.0_deps.rsf', "name": 'hadoop-0.2.0',
-     "relax": 'hadoop-0.2.0_relax_clusters.rsf'}
+     "relax": 'hadoop-0.2.0_relax_clusters.rsf', "ucc": "hadoop-0.2.0-Java_outfile.csv"}
 ]
 
 for version in files:
@@ -16,9 +16,11 @@ for version in files:
     dep = version.get("dep", None)
     relax = version.get("relax", None)
     name = version.get("name", None)
+    ucc = version.get("ucc", None)
 
-    if acdc is not None and relax is not None:
-        generate_class_json(acdc, relax, name + "-class.json")
+    if acdc is not None and relax is not None and name is not None and ucc is not None:
+        ucc_size = read_ucc_output(ucc)
+        generate_class_json(acdc, relax, ucc_size, name + "-class.json")
     if acdc is not None and dep is not None and name is not None:
         m, _, cnt = read_cluster(acdc)
         d = read_deps(dep)
